42c88da1b1d5853344fb1a12bdee420201caadbf
Karthi6556 <karthi.6556@hotmail.com>
2014-10-26 00:46:54 +0530

    Modified in Create and dbConnect

--- Create.php
+++ Create.php
@@ -1,12 +1,22 @@
 <?php
-$host="localhost";
-$username="wwwphpde_admin";
-$password="fischer72";
-$db_name="wwwphpde_db";
+//Includes Localhost Configuration
+include'dbConnect.php';
+//Error Handling
 try{
+//PDO(PHP Data Object) Database Connectivity Initialized
 $pdo=new PDO("mysql:dbname=$db_name;host=$host","$username","$password");
-$statement=$pdo->prepare("CREATE TABLE IF NOT EXISTS `products`(`id` INT(10) NOT NULL AUTO_INCREMENT,`products` VARCHAR(30),`quantity` VARCHAR(10),`price` INT(10),PRIMARY KEY(`id`))");
-$statement->execute();
+//User Table Creation
+$user_creation=$pdo->prepare("CREATE TABLE IF NOT EXISTS `users`(`user_id` VARCHAR(15) NOT NULL,`user_name` VARCHAR(50),`user_designation` VARCHAR(30),`user_department` VARCHAR(30),`user_joined` VARCHAR(15),PRIMARY KEY(`user_id`))");
+$user_creation->execute();
+echo'Users Table Created Successfully<br>';
+//Roles Table Creation
+$role_creation=$pdo->prepare("CREATE TABLE IF NOT EXISTS `roles`(`role_id` VARCHAR(15) NOT NULL,`role_name` VARCHAR(30),`role_purpose` VARCHAR(50),PRIMARY KEY(`role_id`))");
+$role_creation->execute();
+echo'Roles Table Created Successfully<br>';
+//Features Table Creation
+$feature_creation=$pdo->prepare("CREATE TABLE IF NOT EXISTS `features`(`features_id` VARCHAR(15) NOT NULL,`features_name` VARCHAR(30),`features_descr` VARCHAR(30),`features_perm` VARCHAR(5),PRIMARY KEY(`features_id`))");
+$feature_creation->execute();
+echo'Features Table Created Successfully<br>';
 }
 catch(PDOException $e){
  echo'Connection failed '.$e->getMessage();
--- dbConnect.php
+++ dbConnect.php
@@ -1,10 +1,6 @@
 <?php
-$mysql_server = 'localhost';
-$mysql_username = 'root';
-$mysql_password = 'fischer72';
-$mysql_database = 'db_ticket';
-$mysql_table = 'tb_signup';
-$con=mysqli_connect($mysql_server,$mysql_username,$mysql_password,$mysql_database);
-$username = $_POST['username'];
-$password = $_POST['password'];
+$host="localhost";
+$username="root";
+$password="fischer72";
+$db_name="db_acl";
 ?>
