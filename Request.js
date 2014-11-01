56fb61d07b8c1de6eb30e1dad77576082c5afcb9
Karthi6556 <karthi.6556@hotmail.com>
2014-11-01 13:36:16 +0530

    Modified

--- Request.js
+++ Request.js
@@ -25,7 +25,7 @@ $scope.users_disp=function(){
  $scope.post_users_data=function(){
   $http({
    method:'POST',
-   url:'http://localhost:8080/GitHub/ACL/Post_Users_Data.php',
+   url:'http://www.phpdel.info/ACL/Post_Users_Data.php',
    data:{'user_name':$scope.user_name,'user_designation':$scope.user_designation,'user_department':$scope.user_department,'user_joined':$scope.user_joined},
    headers:{'Content-Type':'text/plain'}
   })
@@ -55,7 +55,7 @@ $scope.roles_disp=function(){
  $scope.post_roles_data=function(){
   $http({
    method:'POST',
-   url:'http://localhost:8080/GitHub/ACL/Post_Roles_Data.php',
+   url:'http://www.phpdel.info/ACL/Post_Roles_Data.php',
    data:{'role_name':$scope.role_name,'role_purpose':$scope.role_purpose},
    headers:{'Content-Type':'text/plain'}
   })
@@ -86,7 +86,7 @@ $scope.features_disp=function(){
  $scope.post_features_data=function(){
   $http({
    method:'POST',
-   url:'http://localhost:8080/GitHub/ACL/Post_Features_Data.php',
+   url:'http://www.phpdel.info/ACL/Post_Features_Data.php',
    data:{'features_name':$scope.features_name,'features_descr':$scope.features_descr,'features_perm':$scope.features_perm},
    headers:{'Content-Type':'text/plain'}
   })
@@ -112,7 +112,7 @@ $scope.features_disp=function(){
  $scope.role_features_data=function(){
   $http({
    method:'POST',
-   url:'http://localhost:8080/GitHub/ACL/Post_Role_Features_Data.php',
+   url:'http://www.phpdel.info/ACL/Post_Role_Features_Data.php',
    data:{'role_id':$scope.role_id,'features_id':$scope.features_id},
    headers:{'Content-Type':'text/plain'}
   })
@@ -130,7 +130,7 @@ $scope.features_disp=function(){
  $scope.role_users_data=function(){
   $http({
    method:'POST',
-   url:'http://localhost:8080/GitHub/ACL/Post_Role_Users_Data.php',
+   url:'http://www.phpdel.info/ACL/Post_Role_Users_Data.php',
    data:{'role_id':$scope.role_id,'user_id':$scope.user_id},
    headers:{'Content-Type':'text/plain'}
   })
@@ -147,7 +147,7 @@ $scope.features_disp=function(){
 //GET Method 
  $http({
 	   method:'GET',
-	   url:'http://localhost:8080/GitHub/ACL/Get_Data.php',
+	   url:'http://www.phpdel.info/ACL/Get_Data.php',
 	   headers:{'Content-Type':'text/plain'} 
 	   })
  .success(function(data,status)
